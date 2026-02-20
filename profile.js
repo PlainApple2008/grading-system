b3a7f2d962ab4e9582ec6d4cd59a598495d70eab
PlainApple2008 <danielderrickmcalisura@gmail.com>
2026-02-20 23:12:46 +0800

    - implemented add/remove subjects in profile system - added new theme color

--- profile.js
+++ profile.js
@@ -7,21 +7,77 @@
 
 // TODO Implement profile system
 
-// Displaying Saved Profiles
-function displaySavedProfiles() {
- 
+// Save State
+function loadSavedProfiles() {
+  
 }
 
 
 // Interface controls
+function openAddSubjectInterface() {
+  document.querySelector("#group-add-subject-interface").style.display = "block";
+}
+
+function closeAddSubjectInterface() {
+  // Close interface
+  document.querySelector("#group-add-subject-interface").style.display = "none";
+}
+
+function createSubject() {
+  let subjectName = document.querySelector("#input-new-subject-name").value;
+  
+  if (subjectName.trim().length === 0) {
+    
+    alert("Please enter a name for creating new subject");
+    return;
+  }
+  
+  let p = document.createElement("p");
+  
+  p.textContent = subjectName;
+  
+  let btnDelete = document.createElement("button");
+  
+  btnDelete.textContent = "X";
+  btnDelete.addEventListener("click", (event) => {
+    let parent = btnDelete.parentNode;
+    let children = parent.childNodes;
+    
+    for (let i = 0; i < children.length; i++) {
+      children[i].remove();
+    }
+    
+    parent.remove();
+  });
+  
+  let div = document.createElement("div");
+  
+  div.classList.add("subject");
+  div.appendChild(p);
+  div.appendChild(btnDelete);
+  
+  let sublist = document.querySelector("#subject-list");
+  
+  sublist.appendChild(div);
+  
+  // Clear input
+document.querySelector("#input-new-subject-name").value = "";
+
+  closeAddSubjectInterface();
+}
+
+function viewProfile() {
+  
+}
+
 function deleteCurrentProfile() {
- 
+  
 }
 
 function createProfile() {
- 
+  
 }
 
 function saveCurrentChanges() {
- 
+  
 }
